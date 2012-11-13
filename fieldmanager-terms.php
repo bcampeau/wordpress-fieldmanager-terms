bdf9cdbf6b97974990fd3a2612577036d66600f2
bcampeau <brad@alleyinteractive.com>
2012-11-13 13:56:22 -0500

    Initial checkin

---
+++ fieldmanager-terms.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package Fieldmanager-Terms
+ * @version 0.1
+ */
+/*
+Plugin Name: Fieldmanager Terms
+Plugin URI: http://github.com/bcampeau/fieldmanager-terms
+Description: Adds automatic term extraction from post content for Fieldmanager taxonomy-based fields
+Author: Bradford Campeau-Laurion
+Version: 0.1
+Author URI: http://www.alleyinteractive.com/
+*/
+
+require_once( dirname( __FILE__ ) . '/php/class-fieldmanager-terms.php' );
